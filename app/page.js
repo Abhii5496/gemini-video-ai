241bcf2f0f12245c2a35b076249366d7bf38b040
Abhii <abhishekhsharma2002@gmail.com>
2024-10-28 15:32:33 +0530

    working with diffrent models

--- app/page.js
+++ app/page.js
@@ -1,10 +1,39 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import HomePage from "@/components/Home/HomePage";
-import React from "react";
+import { SelectModel } from "@/components/gemini-model";
 
-export default function page() {
+export default function Page() {
   return (
-    <div className="">
-      <HomePage />
-    </div>
+    <SidebarProvider>
+      {/* <AppSidebar /> */}
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 sticky top-0 left-0 w-full bg-background z-[99999]">
+          <div className="flex items-center gap-2 px-4">
+            {/* <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" /> */}
+            <p className="text-sm text-sidebar-primary-foreground">
+              Start your journey with
+            </p>
+            <SelectModel />
+          </div>
+        </header>
+
+        <HomePage />
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
